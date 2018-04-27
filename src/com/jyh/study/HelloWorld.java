6dd60ceb64d67e6bde33abe32d0fd9851aaae05a
jieyinghao <602346414@qq.com>
2018-04-27 20:50:50 +0800

    第一次提交

---
+++ src/com/jyh/study/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.jyh.study;
+/**
+ * 
+ * @desc 
+ * 
+ * @Author jieyinghao
+ * @2018年4月27日 下午8:49:35 
+ 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
